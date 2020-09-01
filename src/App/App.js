16396926edf066a92b30e7ab6c19666fe1f6c6d8
Joshua Medlen <joshua246@gmail.com>
2020-08-31 19:17:26 -0500

    added inital setup for capstone project

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Gamer Log</h2>
+        <button className="btn btn-info">Let's Play</button>
+      </div>
+    );
+  }
+}
+
+export default App;
