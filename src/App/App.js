c899002840588621700c99e5e4877f20de376afe
Joshua Medlen <joshua246@gmail.com>
2020-09-12 12:28:17 -0500

    created all components for future use. Got auth working. Working on basic navbar now

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.scss';
+// import firebase from 'firebase/app';
+import 'firebase/auth';
+import fbConnection from '../helpers/data/connections';
+import Auth from '../components/pages/Auth/Auth';
+
+fbConnection();
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h2>Gamer Log</h2>
-        <button className="btn btn-info">Let's Play</button>
+        <Auth />
       </div>
     );
   }
